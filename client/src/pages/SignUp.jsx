c2f70be4f82606926a700fe025313aca19587d2f
Avirup Saha <avirup.saha@epsilon.com>
2024-04-05 17:17:24 +0530

    add functionality to signup page

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,44 @@
-import { Link } from "react-router-dom";
-import { Button, Label, TextInput } from "flowbite-react";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
 
 function SignUp() {
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handelChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+  };
+
+  const handelSubmit = async (e) => {
+    e.preventDefault();
+    if (!formData?.username || !formData?.email || !formData?.password) {
+      return setErrorMessage("Please fill out all fields");
+    }
+    try {
+      setLoading(true);
+      setErrorMessage(null);
+      const res = await fetch('/api/auth/signup', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        return setErrorMessage(data?.message);
+      }
+      setLoading(false);
+      if(res.ok){
+        navigate("/signin");
+      }
+    } catch (error) {
+      setErrorMessage(error.message);
+      setLoading(false);
+    }
+  };
+
   return (
     <div className="min-h-screen mt-20">
       <div className=" flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
@@ -21,34 +58,48 @@ function SignUp() {
 
         {/* right side */}
         <div className="flex-1">
-          <form className="flex flex-col gap-4">
-              <div>
-                <Label value="Your username" />
-                <TextInput
-                  type="text"
-                  placeholder="enter your username"
-                  id="username"
-                />
-              </div>
-              <div>
-                <Label value="Your email" />
-                <TextInput
-                  type="text"
-                  placeholder="enter your email"
-                  id="email"
-                />
-              </div>
-              <div>
-                <Label value="Your password" />
-                <TextInput
-                  type="text"
-                  placeholder="enter password"
-                  id="password"
-                />
-              </div>
-              <Button gradientDuoTone="purpleToPink" type="submit">
-                Sign Up
-              </Button>
+          <form className="flex flex-col gap-4" onSubmit={handelSubmit}>
+            <div>
+              <Label value="Your username" />
+              <TextInput
+                type="text"
+                placeholder="enter your username"
+                id="username"
+                onChange={handelChange}
+              />
+            </div>
+            <div>
+              <Label value="Your email" />
+              <TextInput
+                type="email"
+                placeholder="enter your email"
+                id="email"
+                onChange={handelChange}
+              />
+            </div>
+            <div>
+              <Label value="Your password" />
+              <TextInput
+                type="password"
+                placeholder="enter password"
+                id="password"
+                onChange={handelChange}
+              />
+            </div>
+            <Button
+              gradientDuoTone='purpleToPink'
+              type='submit'
+              disabled={loading}
+            >
+              {loading ? (
+                <>
+                  <Spinner size='sm' />
+                  <span className='pl-3'>Loading...</span>
+                </>
+              ) : (
+                'Sign Up'
+              )}
+            </Button>
           </form>
           <div className="flex gap-2 text-sm mt-4">
             <span className="font-semibold">Have an account?</span>
@@ -56,6 +107,11 @@ function SignUp() {
               Sign In
             </Link>
           </div>
+          {errorMessage && (
+            <Alert className='mt-5' color='failure'>
+              {errorMessage}
+            </Alert>
+          )}
         </div>
       </div>
     </div>
