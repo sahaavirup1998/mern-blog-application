b1b099647ba65f5ec303ec3a07a8b44e8171e661
Avirup Saha <avirup.saha@epsilon.com>
2024-04-09 14:02:00 +0530

    create signin functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,9 @@
 import User from "../models/user.model.js"
 import bcryptjs from "bcryptjs";
 import { errorHandeler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
+// signup functionality
 export const signup = async (req, res, next) => {
     const{ username, email, password } = req.body;
 
@@ -21,6 +23,35 @@ export const signup = async (req, res, next) => {
         await newUser.save();
         res.json("Signup successful");
     } catch(error){
+        next(error);
+    }
+}
+
+// signin functionality
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+    
+    if(!email || !password || email === "" || password === "") {
+        next(errorHandeler(400, "All fields are required"));
+    }
+
+    try {
+        const validUser = await User.findOne({ email });
+        if(!validUser) {
+            return next(errorHandeler(404, "User not found"));
+        };
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if(!validPassword) {
+            return next(errorHandeler(400, "Invalid Password"));
+        };
+        const token = jwt.sign(
+           { id: validUser._id }, process.env.JWT_SECRET, 
+        );
+        const { password: pass, ...rest } = validUser._doc;
+        res.status(200).cookie("access_token", token, {
+            httpOnly: true
+        }).json(rest);
+    } catch (error) {
         next(error);
     }
 }
