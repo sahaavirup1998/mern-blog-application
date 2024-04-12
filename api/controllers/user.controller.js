ecf9072d0d3265b8e481b622f014b0708536a19c
Avirup Saha <avirup.saha@epsilon.com>
2024-04-13 03:09:53 +0530

    add delete user api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -6,6 +6,7 @@ export const test = (req, res) => {
   res.json({ message: "API is working" });
 };
 
+// update user functionality
 export const updateUser = async (req, res, next) => {
   if (req.user.id !== req.params.userId) {
     return next(errorHandeler(403, "You are not allowed to update this user!"));
@@ -36,22 +37,35 @@ export const updateUser = async (req, res, next) => {
       );
     }
   }
-    try {
-      const updatedUser = await User.findByIdAndUpdate(
-        req.params.userId,
-        {
-          $set: {
-            username: req.body.username,
-            email: req.body.email,
-            profilePicture: req.body.profilePicture,
-            password: req.body.password,
-          },
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.userId,
+      {
+        $set: {
+          username: req.body.username,
+          email: req.body.email,
+          profilePicture: req.body.profilePicture,
+          password: req.body.password,
         },
-        { new: true }
-      );
-      const { password, ...rest } = updatedUser._doc;
-      res.status(200).json(rest);
-    } catch (error) {
-      next(error);
-    }
-  };
+      },
+      { new: true }
+    );
+    const { password, ...rest } = updatedUser._doc;
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
+
+// delete user functionality
+export const deleteUser = async (req, res, next) => {
+  if (req.user.id !== req.params.userId){
+    return next (errorHandeler(403, "You are not allowed to delete this user!"));
+  }
+  try {
+    await User.findByIdAndDelete(req.params.userId);
+    res.status(200).json({ message: "User deleted successfully" });
+  } catch (error) {
+    next(error);
+  }
+};
