7b18d5b13064ccea504047b39246ab3b8c100faf
Avirup Saha <avirup.saha@epsilon.com>
2024-04-14 13:18:43 +0530

    update profile and create post page and firebase

--- client/src/components/DProfile.jsx
+++ client/src/components/DProfile.jsx
@@ -22,7 +22,7 @@ import {
 import { HiOutlineExclamationCircle } from "react-icons/hi";
 import {Link} from "react-router-dom";
 
-export default function DProfile() {
+export default function DashProfile() {
   const { currentUser, error } = useSelector((state) => state.user);
   const [imageFile, setImageFile] = useState(null);
   const [imageFileUrl, setImageFileUrl] = useState(null);
@@ -35,7 +35,6 @@ export default function DProfile() {
   const [formData, setFormData] = useState({});
   const filePickerRef = useRef();
   const dispatch = useDispatch();
-
   const handleImageChange = (e) => {
     const file = e.target.files[0];
     if (file) {
@@ -43,7 +42,6 @@ export default function DProfile() {
       setImageFileUrl(URL.createObjectURL(file));
     }
   };
-
   useEffect(() => {
     if (imageFile) {
       uploadImage();
@@ -67,7 +65,7 @@ export default function DProfile() {
       },
       (error) => {
         setImageFileUploadError(
-          'Could not upload image (File must be less than 2MB!)'
+          'Could not upload image (File must be less than 2MB)'
         );
         setImageFileUploadProgress(null);
         setImageFile(null);
@@ -97,7 +95,7 @@ export default function DProfile() {
       return;
     }
     if (imageFileUploading) {
-      setUpdateUserError('Please wait for image to upload!');
+      setUpdateUserError('Please wait for image to upload');
       return;
     }
     try {
@@ -122,7 +120,6 @@ export default function DProfile() {
       setUpdateUserError(error.message);
     }
   };
-
   const handleDeleteUser = async () => {
     setShowModal(false);
     try {
@@ -156,20 +153,19 @@ export default function DProfile() {
       console.log(error.message);
     }
   };
-
   return (
-    <div className="max-w-lg mx-auto p-3 w-full">
-      <h1 className="my-7 text-center font-bold text-3xl">Profile</h1>
-      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+    <div className='max-w-lg mx-auto p-3 w-full'>
+      <h1 className='my-7 text-center font-semibold text-3xl'>Profile</h1>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
         <input
-          type="file"
-          accept="image/*"
+          type='file'
+          accept='image/*'
           onChange={handleImageChange}
           ref={filePickerRef}
           hidden
         />
         <div
-          className="relative w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full"
+          className='relative w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full'
           onClick={() => filePickerRef.current.click()}
         >
           {imageFileUploadProgress && (
@@ -179,9 +175,9 @@ export default function DProfile() {
               strokeWidth={5}
               styles={{
                 root: {
-                  width: "100%",
-                  height: "100%",
-                  position: "absolute",
+                  width: '100%',
+                  height: '100%',
+                  position: 'absolute',
                   top: 0,
                   left: 0,
                 },
@@ -195,71 +191,76 @@ export default function DProfile() {
           )}
           <img
             src={imageFileUrl || currentUser.profilePicture}
-            alt="user"
-            className={`rounded-full w-full h-full  object-cover border-8 border-[lightgray] ${
+            alt='user'
+            className={`rounded-full w-full h-full object-cover border-8 border-[lightgray] ${
               imageFileUploadProgress &&
               imageFileUploadProgress < 100 &&
-              "opacity-60"
+              'opacity-60'
             }`}
           />
         </div>
         {imageFileUploadError && (
-          <Alert color="failure">{imageFileUploadError}</Alert>
+          <Alert color='failure'>{imageFileUploadError}</Alert>
         )}
         <TextInput
-          type="text"
-          id="username"
-          placeholder="username"
+          type='text'
+          id='username'
+          placeholder='username'
           defaultValue={currentUser.username}
           onChange={handleChange}
         />
         <TextInput
-          type="email"
-          id="email"
-          placeholder="email"
+          type='email'
+          id='email'
+          placeholder='email'
           defaultValue={currentUser.email}
           onChange={handleChange}
         />
         <TextInput
-          type="password"
-          id="password"
-          placeholder="password"
+          type='password'
+          id='password'
+          placeholder='password'
           onChange={handleChange}
         />
-        <Button type="submit" gradientDuoTone="purpleToBlue" outline>
+        <Button
+          type='submit'
+          gradientDuoTone='purpleToBlue'
+          outline
+        >
           Update
         </Button>
-        {
-          currentUser.isAdmin && (
-            <Link to={"/create-post"}>
-              <Button type="button" gradientDuoTone="purpleToPink" className="w-full">
+        {currentUser.isAdmin && (
+          <Link to={'/create-post'}>
+            <Button
+              type='button'
+              gradientDuoTone='purpleToPink'
+              className='w-full'
+            >
               Create a post
-              </Button>
-            </Link>
-              
-          )
-        }
+            </Button>
+          </Link>
+        )}
       </form>
-      <div className="text-red-500 flex justify-between mt-5">
-        <span className="cursor-pointer" onClick={() => setShowModal(true)}>
+      <div className='text-red-500 flex justify-between mt-5'>
+        <span onClick={() => setShowModal(true)} className='cursor-pointer'>
           Delete Account
         </span>
-        <span onClick={handleSignout} className="cursor-pointer">
+        <span onClick={handleSignout} className='cursor-pointer'>
           Sign Out
         </span>
       </div>
       {updateUserSuccess && (
-        <Alert color="success" className="mt-5">
+        <Alert color='success' className='mt-5'>
           {updateUserSuccess}
         </Alert>
       )}
       {updateUserError && (
-        <Alert color="failure" className="mt-5">
+        <Alert color='failure' className='mt-5'>
           {updateUserError}
         </Alert>
       )}
       {error && (
-        <Alert color="failure" className="mt-5">
+        <Alert color='failure' className='mt-5'>
           {error}
         </Alert>
       )}
@@ -267,20 +268,20 @@ export default function DProfile() {
         show={showModal}
         onClose={() => setShowModal(false)}
         popup
-        size="md"
+        size='md'
       >
         <Modal.Header />
         <Modal.Body>
-          <div className="text-center">
-            <HiOutlineExclamationCircle className="h-14 w-14 text-gray-400 dark:text-gray-200 mb-4 mx-auto" />
-            <h3 className="mb-5 text-lg text-gray-500 dark:text-gray-400">
+          <div className='text-center'>
+            <HiOutlineExclamationCircle className='h-14 w-14 text-gray-400 dark:text-gray-200 mb-4 mx-auto' />
+            <h3 className='mb-5 text-lg text-gray-500 dark:text-gray-400'>
               Are you sure you want to delete your account?
             </h3>
-            <div className="flex justify-center gap-4">
-              <Button color="failure" onClick={handleDeleteUser}>
+            <div className='flex justify-center gap-4'>
+              <Button color='failure' onClick={handleDeleteUser}>
                 Yes, I&apos;m sure
               </Button>
-              <Button color="gray" onClick={() => setShowModal(false)}>
+              <Button color='gray' onClick={() => setShowModal(false)}>
                 No, cancel
               </Button>
             </div>
