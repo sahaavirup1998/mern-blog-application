95c7990214a0eb99fe7a93018e30856eb3928d44
Avirup Saha <avirup.saha@epsilon.com>
2024-04-17 22:32:58 +0530

    update post route page

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -34,16 +34,16 @@ export const getposts = async (req, res, next) => {
     const limit = parseInt(req.query.limit) || 9;
     const sortDirection = req.query.order === 'asc' ? 1 : -1;
     const posts = await Post.find({
-      // ...(req.query.userId && { userId: req.query.userId }),
-      // ...(req.query.category && { category: req.query.category }),
-      // ...(req.query.slug && { slug: req.query.slug }),
-      // ...(req.query.postId && { _id: req.query.postId }),
-      // ...(req.query.searchTerm && {
-      //   $or: [
-      //     { title: { $regex: req.query.searchTerm, $options: 'i' } },
-      //     { content: { $regex: req.query.searchTerm, $options: 'i' } },
-      //   ],
-      // }),
+      ...(req.query.userId && { userId: req.query.userId }),
+      ...(req.query.category && { category: req.query.category }),
+      ...(req.query.slug && { slug: req.query.slug }),
+      ...(req.query.postId && { _id: req.query.postId }),
+      ...(req.query.searchTerm && {
+        $or: [
+          { title: { $regex: req.query.searchTerm, $options: 'i' } },
+          { content: { $regex: req.query.searchTerm, $options: 'i' } },
+        ],
+      }),
     })
       .sort({ updatedAt: sortDirection })
       .skip(startIndex)
