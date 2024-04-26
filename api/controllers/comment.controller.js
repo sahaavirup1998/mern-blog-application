5b405049e741c4c2d6fda8caf47124aee645a65f
Avirup Saha <avirup.saha@epsilon.com>
2024-04-26 16:34:58 +0530

    add edit functionality to the comment

--- api/controllers/comment.controller.js
+++ api/controllers/comment.controller.js
@@ -58,3 +58,28 @@ export const likeComment = async (req, res, next) => {
     next(error);
   }
 };
+
+// edit comment functionality
+export const editComment = async (req, res, next) => {
+  try {
+    const comment = await Comment.findById(req.params.commentId);
+    if(!comment){
+      return next(errorHandeler(403, "Comment not found!"));
+    }
+    if(comment.userId !== req.user.id && !req.user.isAdmin) {
+      return next(errorHandeler(403, "You are not allowed to edit the comment"));
+    }
+    const editedComment = await Comment.findByIdAndUpdate(
+      req.params.commentId, 
+      {
+        content: req.body.content,
+      },
+      {
+        new: true
+      }
+    );
+    res.status(200).json(editedComment);
+  } catch (error) {
+    next(error);
+  }
+}
